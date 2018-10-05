c4ee4adb8e7570b3ec2aa8358a37783315bda09d
kwondonghyen <dhdhkk3@gmail.com>
2018-10-05 11:08:18 +0900

    접속 및 데이터 수신 확인 버전

---
+++ CRT_TCP_Client/CRT_TCP_Client/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CRT_TCP_Client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LogTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTest);
+            this.groupBox1.Controls.Add(this.btnEdit);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.tbPort);
+            this.groupBox1.Controls.Add(this.tbIp);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "설정";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(241, 94);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 6;
+            this.btnTest.Text = "TEST";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(146, 94);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "접속 해제";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(51, 94);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "접속";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(147, 48);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(108, 21);
+            this.tbPort.TabIndex = 3;
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(147, 18);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(108, 21);
+            this.tbIp.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Location = new System.Drawing.Point(0, 134);
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(366, 439);
+            this.LogTextBox.TabIndex = 1;
+            this.LogTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 585);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "교통량 모니터링 ";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox LogTextBox;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ CRT_TCP_Client/CRT_TCP_Client/MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace CRT_TCP_Client
+{
+    public partial class MainForm : Form
+    {
+        TcpClient tc;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tc = new TcpClient(tbIp.Text.ToString(), Convert.ToInt32(tbPort.Text));
+                if (tc.Connected)
+                {
+                    LogTextBox.AppendText("접속 - IP :" + tbIp.Text.ToString() + " / Port : " + Convert.ToInt32(tbPort.Text) + "\r\n");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("접속 오류");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            tc.Close();
+            LogTextBox.AppendText("연결 해제 " + "\r\n");
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            byte[] arrBYTE = new byte[10];
+
+            // 요청 날짜를 맞추기.
+            // start time (YY-MM-DD hh:mm)
+            arrBYTE[0] = (byte)18;
+            arrBYTE[1] = (byte)6;
+            arrBYTE[2] = (byte)5;
+            arrBYTE[3] = (byte)14;
+            arrBYTE[4] = (byte)00;
+
+            // end time (YY-MM-DD hh:mm)
+            arrBYTE[5] = (byte)18;
+            arrBYTE[6] = (byte)6;
+            arrBYTE[7] = (byte)6;
+            arrBYTE[8] = (byte)13;
+            arrBYTE[9] = (byte)59;
+
+            NetworkStream stream = tc.GetStream();
+            stream.Write(arrBYTE, 0, arrBYTE.Length);
+            stream.Flush();
+           
+            int length;
+            byte[] outbuf = new byte[1024];
+
+            string fileName = @"C:\WHELMON\CMDB\out.txt";
+
+            FileStream fis = new FileStream(fileName, FileMode.Create);
+
+            while ((length = stream.Read(outbuf, 0, outbuf.Length)) > 0)
+            {
+                fis.Write(outbuf, 0, length);
+            }
+
+            fis.Close();
+
+            int nbytes = stream.Read(outbuf, 0, outbuf.Length);
+            string output = Encoding.ASCII.GetString(outbuf, 0, nbytes);
+
+            stream.Close();
+            tc.Close();
+
+            LogTextBox.AppendText($"{nbytes} bytes: {output}");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Test용 초기 세팅값 지정
+            tbIp.Text = "192.168.10.199";
+            tbPort.Text = "8017";
+        }
+    }
+}
